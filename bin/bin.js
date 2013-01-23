a144a5fcd51d1166937999b5abc2c50d7c65a91c
stoke <stoke95@gmail.com>
2013-01-23 15:56:38 +0000

    middlewares

--- bin/bin.js
+++ bin/bin.js
@@ -4,23 +4,33 @@ var optimist = require('optimist');
 optimist.alias('d', 'directory-listing');
 optimist.alias('p', 'port');
 optimist.alias('D', 'directory');
+optimist.alias('i', 'index');
 optimist.alias('h', 'help');
+optimist.alias('u', 'username');
+optimist.alias('P', 'password');
 
 optimist.boolean('d');
 optimist.boolean('h');
 optimist.string('D');
 optimist.string('p');
+optimist.string('i');
+optimist.string('u');
+optimist.string('P');
 
 optimist.describe('d', 'Activate directory listing');
 optimist.describe('p', 'Port');
 optimist.describe('D', 'Main directory');
 optimist.describe('q', 'Quiet');
 optimist.describe('h', 'Show help');
+optimist.describe('i', 'Index file (e.g. index.html)');
+optimist.describe('u', 'Username [default: none]');
+optimist.describe('P', 'Password [default: none]');
 
 optimist.default('d', false);
 optimist.default('D', '.');
 optimist.default('p', 8000);
 optimist.default('q', false);
+optimist.default('i', 'index.html');
 
 var argv = optimist.argv;
 
@@ -29,9 +39,21 @@ if (argv.h) {
   process.exit(0);
 }
 
+var middlewares = require('../lib/middlewares'),
+    mids = [];
+
+if (argv.u && argv.p)
+  mids.push(middlewares.auth(argv.u, argv.P));
+    
+argv.i = argv.i || 'index.html';
+mids.push(middlewares.index(argv.i));
+
+if (argv.d)
+  mids.push(middlewares.listing);
+
 require('../')({
   path: argv.D || '.',
-  listing: argv.d || false,
   port: argv.p || 8000,
-  quiet: argv.q || false
+  quiet: argv.q || false,
+  middlewares: mids
 });
--- test/test.js
+++ test/test.js
@@ -1,40 +1,42 @@
 var request = require('request'),
     expect = require('expect.js');
 
-var pub = require('../');
+var pub = require('../'),
+    middlewares = require('../lib/middlewares');
 
 before(function() {
   pub({
-    port: 8000,
+  port: 8001,
     path: __dirname + '/public',
     listing: true,
+    middlewares: [middlewares.index('index.html'), middlewares.listing]
   });
 });
 
 describe('server', function() {
   it('should answer requests correctly', function(done) {
-    request('http://localhost:8000/test', function(req, res, body) {
+    request('http://localhost:8001/test', function(req, res, body) {
       expect(body).to.be('nexgay');
       done();
     });
   });
 
   it('should return 404 if file doesn\'t exist', function(done) {
-    request('http://localhost:8000/unexistent', function(req, res, body) {
+    request('http://localhost:8001/unexistent', function(req, res, body) {
       expect(res.statusCode).to.be(404);
       done();
     });
   });
 
   it('should use directory listing if it there isn\'t an index.html', function(done) {
-    request('http://localhost:8000/windextest', function(req, res, body) {
+    request('http://localhost:8001/windextest', function(req, res, body) {
       expect(body).to.contain('<html>');
       done();
     });
   });
 
   it('should use index.html if it\'s there', function(done) {
-    request('http://localhost:8000/indextest', function(req, res, body) {
+    request('http://localhost:8001/indextest', function(req, res, body) {
       expect(body).to.be('yagxen');
       done();
     });
